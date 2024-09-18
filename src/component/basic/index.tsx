a3fe079514b5e4de790eb05c26e1cba05debcffd
Cao Kejian <colinccao@tencent.com>
2024-09-18 13:16:30 +0800

    feat: 完成phone

--- src/component/basic/index.tsx
+++ src/component/basic/index.tsx
@@ -1,8 +1,18 @@
 import React, { FC } from "react"
+import * as s from './index.module.less'
 
 const Basic: FC = () => { 
   return (
-    <div>Basic</div>
+    <div className={s.wrapper}>
+      <div className={s.left}>
+        <div className={s.phone}>
+          <div className={s.content}>
+            <img src="https://gd-hbimg.huaban.com/4533f7b3c3a2daba830b35730b299228d0f7900017687e-gQmslT_fw236" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className={s.right}></div>
+    </div>
   )
 }
 
